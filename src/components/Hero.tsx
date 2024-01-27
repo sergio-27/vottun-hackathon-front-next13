f8f55aba9730a0a096bb93129ee63321100bc6cb
Sebas Arriaza <sebasarriaza@icloud.com>
2024-01-27 19:46:23 +0100

    add login and signup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import heroImage from '../../public/hero-image.png'; // Asegúrate de reemplazar con la ruta correcta
 
 const Hero = () => {
@@ -6,17 +7,16 @@ const Hero = () => {
         <div className="relative min-h-screen flex items-center justify-center bg-black">
             <Image src={heroImage} alt="Hero Background" layout="fill" objectFit="cover" className="opacity-75"/>
             <div className="absolute">
-                <a href="tu-url-de-login">
+                <Link href="/login" passHref>
                     <button className="mb-4 px-10 py-4 bg-sky-500 text-black font-semibold rounded-md hover:bg-sky-600 transition duration-300 ease-in-out transform hover:-translate-y-1">Login</button>
-                </a>
+                </Link>
                 <br/>
-                <a href="tu-url-de-sign-in">
+                <Link href="/signup" passHref>
                     <button className="px-10 py-4 bg-sky-500 text-black font-semibold rounded-md hover:bg-sky-600 transition duration-300 ease-in-out transform hover:-translate-y-1">Sign in</button>
-                </a>
+                </Link>
             </div>
         </div>
     );
 };
-
 
 export default Hero;

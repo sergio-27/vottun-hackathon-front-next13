3be2f6b67fd6b22effda05206faf77f544d7b44b
Sergio <sergio96ruiz@gmail.com>
2024-01-28 10:59:40 +0100

    added contract address and get items from database

--- src/components/CommunityCard.tsx
+++ src/components/CommunityCard.tsx
@@ -5,20 +5,23 @@ interface CommunityCardProps {
   image: string;
   title: string;
   description: string;
+  contractAddress: string;
 }
 
 const CommunityCard: React.FC<CommunityCardProps> = ({
   image,
   title,
   description,
+  contractAddress,
 }) => {
   return (
     <div className="flex justify-center">
     <div className="my-4 flex self-center w-[90%] overflow-hidden rounded-lg bg-gray-800 text-white">
-      <img src={image} alt={title} className="h-24 w-24 object-cover" />
+      <img src={image} alt={title} className="h-24 w-24 object-cover m-3" />
       <div className="px-8 py-4">
         <h2 className="text-xl font-bold">{title}</h2>
         <p>{description}</p>
+        <p>Contract address: {contractAddress}</p>
       </div>
     </div>
     </div>

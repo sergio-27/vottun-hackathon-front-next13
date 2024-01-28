61551eb76ab89a7a09aa51014dfb0ff1cdf9aecd
Sebas Arriaza <sebasarriaza@icloud.com>
2024-01-28 04:11:39 +0100

    Update SolutionCards background

--- src/components/SolutionCard.tsx
+++ src/components/SolutionCard.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 
 interface SolutionCardProps {
-    backgroundImage: string;
-    title: string;
-  }
-  
-  const SolutionCard: React.FC<SolutionCardProps> = ({
-    backgroundImage,
-    title,
-  }) => {
-    return (
-      <div className="mx-auto my-10 w-1/2 overflow-hidde flex justify-center rounded-lg text-white border-2 border-gray-200 hover:scale-120 transition-transform duration-300" 
-           style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-        <div className="px-4 py-2 flex items-center h-24">
-          <h2 className="text-3xl font-bold">{title}</h2>
-        </div>
+  title: string;
+}
+
+const SolutionCard: React.FC<SolutionCardProps> = ({
+  title,
+}) => {
+  return (
+    <div className="mx-auto my-10 w-1/2 overflow-hidden flex justify-center rounded-lg text-white border-2 border-gray-200 hover:scale-120 transition-transform duration-300 bg-gray-800">
+      <div className="px-4 py-2 flex items-center h-24">
+        <h2 className="text-3xl font-bold">{title}</h2>
       </div>
-    );
-  };
-  
-  export default SolutionCard;
+    </div>
+  );
+};
+
+export default SolutionCard;

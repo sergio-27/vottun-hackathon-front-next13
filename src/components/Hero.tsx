080d2305bd306624114b10739f19abbe3d980bb9
Sebas Arriaza <sebasarriaza@icloud.com>
2024-01-28 01:05:30 +0100

    Update Hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import heroImage from '../../public/hero2.png'; 
+import heroImage from '../../public/hero.png'; 
 
 const Hero = () => {
     return (
